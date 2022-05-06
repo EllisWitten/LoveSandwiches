abd205db518f5aa7aae8b8cb2fc8a8f1251b1d0d
EllisWitten <e.witten.2001@gmail.com>
2022-05-06 19:11:36 +0000

    add validation to check that 6 vlaue shave been entered

--- run.py
+++ run.py
@@ -20,9 +20,25 @@ def get_sales_data():
     """
     print('Please enter sales data from the last market.')
     print('Data should be six numbers, seperated by commas')
-    print('Example: 21,63,54,26,85,49')
+    print('Example: 21,63,54,26,85,49\n')
 
     data_str = input('Enter your data here:')
-    print(f'The data provided is ({data_str}).')
+    sales_data = data_str.split(',')
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Converts all the data values cna be converted into integers.
+    Checks that there are 6 numbers.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f'Exactly 6 values required, you provided {len(values)}'
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
 
 get_sales_data()
