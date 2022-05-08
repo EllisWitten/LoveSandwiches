5cd7da9ed1e292207fd10dfeef3860cbc0066e9f
EllisWitten <e.witten.2001@gmail.com>
2022-05-08 23:03:24 +0000

    refactor update worksheet functions into one

--- run.py
+++ run.py
@@ -51,20 +51,11 @@ def validate_data(values):
         return False
     return True
 
-def update_sales_worksheet(data):
-    """
-    update sales worksheet, add new row with the list provided
-    """
-    print('updating sales worksheet...\n')
-    sales_worksheet = SHEET.worksheet('sales')
-    sales_worksheet.append_row(data)
-    print('Sales worksheet updated successfully.\n')
-
 def calculate_surplus_data(sales_row):
     """
     Compare slaes with stock and calculate surplus data
     """
-    print('Calculating surplus data...')
+    print('Calculating surplus data...\n')
     stock = SHEET.worksheet('stock').get_all_values()
     stock_row = stock[-1]
     surplus_data = []
@@ -74,14 +65,15 @@ def calculate_surplus_data(sales_row):
         surplus_data.append(surplus)
     return surplus_data
 
-def update_surplus_worksheet(data):
+def update_worksheet(data,worksheet):
     """
-    update surplus worksheet, add new row with the list provided
+    Takes a list of integers and a worksheet name and
+    updates the appropriate worksheet.
     """
-    print('updating surplus worksheet...\n')
-    surplus_worksheet = SHEET.worksheet('surplus')
-    surplus_worksheet.append_row(data)
-    print('surplus worksheet updated successfully.\n')
+    print(f'Updating {worksheet.capitalize()} worksheet...\n')
+    spec_worksheet = SHEET.worksheet(worksheet)
+    spec_worksheet.append_row(data)
+    print(f'{worksheet.capitalize()} worksheet updated successfully.\n')
 
 def main():
     """
@@ -89,9 +81,9 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data,'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, 'surplus')
 
 print('Welcome to Love Sandwiches Data Automation.\n')
 main()
